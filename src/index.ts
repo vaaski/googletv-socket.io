c359ddf8faecb6701c9e01c87475f85542200406
vaaski <admin@vaa.ski>
2022-10-05 20:50:30 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
-import { GoogleTV } from "googletv"
 import { readFile, writeFile } from "node:fs/promises"
 import path from "path"
 import { fileURLToPath } from "url"
 
+import { GoogleTV } from "googletv"
+import { Server } from "socket.io"
+
+const port = parseInt(process.env.SOCKET_PORT ?? "3000")
+
+const io = new Server({
+  cors: { origin: "*" },
+})
+
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const GOOGLE_TV_ADDR = "192.168.0.132"
+const GOOGLE_TV_ADDR = "gtv"
 const CERT_PATH = path.join(__dirname, "./pairing-cert.json")
 
 let gtv: GoogleTV
@@ -22,9 +30,20 @@ try {
   gtv = new GoogleTV(GOOGLE_TV_ADDR)
 }
 
-await gtv.init()
-await writeFile(CERT_PATH, JSON.stringify(gtv.options.certificate))
+io.on("connection", socket => {
+  console.log("new socket connection")
+  socket.on("code", code => gtv.sendPairingCode(code))
+  socket.on("init", async () => {
+    await gtv.init()
+    await writeFile(CERT_PATH, JSON.stringify(gtv.options.certificate))
+  })
+})
 
-console.log("works!")
+gtv.on("secretCodeRequest", () => {
+  io.emit("secretCodeRequest")
+})
+
+io.listen(port)
+console.log(`listening on ${port}`)
 
 export {}
